99934dd314e2bc698fbd1c1693fa44e02dd75f42
TIANXUAN13 <2926930231@qq.com>
2026-04-05 15:52:21 +0800

    Merge chat migration branches

---
+++ chat/migrations/0031_merge_20260405_0001.py
@@ -0,0 +1,11 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("chat", "0029_siteconfiguration_allowed_hosts"),
+        ("chat", "0030_delete_mobiledevice"),
+    ]
+
+    operations = []
